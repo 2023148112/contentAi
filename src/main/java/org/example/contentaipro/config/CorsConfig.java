997b6fe71748461a15644b91d9a2811010a063f0
2023148112 <he2894132518@yonsei.ac.kr>
2026-04-17 02:17:29 +0900

    Update CorsConfig.java

--- src/main/java/org/example/contentaipro/config/CorsConfig.java
+++ src/main/java/org/example/contentaipro/config/CorsConfig.java
@@ -1,25 +1,21 @@
 package org.example.contentaipro.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
+public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true)//允许浏览器把 Cookie 带过去
-                .allowedOriginPatterns("*")
-                .allowedMethods("get", "post", "put", "delete", "head", "options")
-               .allowedOrigins("https://content-ai-seven-delta.vercel.app")
-        /*每个方法什么意思：
-        方法	含义
-        GET	查数据
-        POST	创建
-        PUT	更新
-        DELETE	删除*/
-                .allowedHeaders("*").exposedHeaders("*");//允许所有请求头和响应头
+                .allowCredentials(true))//允许浏览器把 Cookie 带过去
+                .allowedOrigins(
+                        "https://content-ai-seven-delta.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 }
